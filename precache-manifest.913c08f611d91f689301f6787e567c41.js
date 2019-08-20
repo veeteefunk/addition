e2089b1717f2906721875eabf8c4ccc2ab3c2d66
veeteefunk <victormarinviloria@gmail.com>
2019-08-20 19:49:56 +0200

    Updates

---
+++ precache-manifest.913c08f611d91f689301f6787e567c41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c4cc1d6f4e3a094cc640029c527506",
+    "url": "/addition/index.html"
+  },
+  {
+    "revision": "a385ef332340fe1f96ba",
+    "url": "/addition/static/css/main.82acd0c3.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/addition/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a385ef332340fe1f96ba",
+    "url": "/addition/static/js/main.1efdc0e1.chunk.js"
+  },
+  {
+    "revision": "409d53552421998daa40",
+    "url": "/addition/static/js/runtime~main.ad54002c.js"
+  }
+]);
